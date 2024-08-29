910b3492a29b1868821ff0f6983f4ec0c4b166be
jpfelixx <felix.joao@aluno.ifsp.edu.br>
2024-08-29 16:45:35 -0300

    adição dos exercícios 11 e 12

---
+++ JavaTokio2024/src/exercicios/aula06/ex12.java
@@ -0,0 +1,20 @@
+package exercicios.aula06;
+
+package exercicios.aula06;
+
+import java.util.Scanner;
+
+public class ex12 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner (System.in);
+		float ladoa,ladob,ladoc;
+		
+	
+		
+		scan.close();
+
+		
+	}
+}
